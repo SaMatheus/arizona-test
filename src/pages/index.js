c83e1184ebb45c546113c77f6fd1e263f30d6a17
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-06 14:12:57 -0300

    JoinEmailContainer finalizado

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 // STYLES
 import styles from '../styles/pages/Home.module.css'
 
+// HOOKS
+import { useState } from 'react'
+
 // COMPONENTS
 import Header from '../components/Header'
 import Button from '../components/Button'
@@ -8,10 +11,30 @@ import Button from '../components/Button'
 // ICONS
 import { DiApple, DiAndroid } from "react-icons/di";
 import { FiPlay } from "react-icons/fi";
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa"
+
+// DATA
 import particularsData from '../data/particularsData';
+import testimonialData from '../data/testimonialData';
 
 
 export default function Home() {
+  const [current, setCurrent] = useState(0)
+  const length = testimonialData.length
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const handleSubmit = () => {
+    alert('Email successfully sent!')
+  }
+
   return (
     <>
       <Header />
@@ -90,6 +113,55 @@ export default function Home() {
           )
         })}
       </section>
+      <section className={styles.testimonialContainer}>
+        <div className={styles.testimonialCarrousel}>
+        <BiLeftArrow className={styles.leftArrow} onClick={prevSlide}/>
+        <BiRightArrow className={styles.rightArrow} onClick={nextSlide}/>
+        {testimonialData.map((slide, index) => (
+            <div key={slide.id}
+              className={styles.testimony}
+            >
+              {index === current && (
+                <>
+                  <p>
+                    <FaQuoteLeft />
+                      {slide.testimony}
+                    <FaQuoteRight />
+                  </p>
+                  <strong>{slide.author}</strong>
+                  <span>{slide.job}</span>
+                </>
+              )}
+              <div className={styles.carrouselIndex}>
+                <span 
+                  className={current === 0 ? `${styles.carrouselIndexSpan} ${styles.active}` : `${styles.carrouselIndexSpan}`}
+                  onClick={() => setCurrent(0)}
+                />
+                <span 
+                  className={current === 1 ? `${styles.carrouselIndexSpan} ${styles.active}` : `${styles.carrouselIndexSpan}`}
+                  onClick={() => setCurrent(1)}
+                />
+                  <span 
+                  className={current === 2 ? `${styles.carrouselIndexSpan} ${styles.active}` : `${styles.carrouselIndexSpan}`}
+                  onClick={() => setCurrent(2)}
+                />
+                <span 
+                  className={current === 3 ? `${styles.carrouselIndexSpan} ${styles.active}` : `${styles.carrouselIndexSpan}`}
+                  onClick={() => setCurrent(3)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className={styles.joinEmailContainer}>
+        <h2>Join our liste and stay update</h2>
+        <label htmlFor="">
+          <input type="text" placeholder="Enter your email"/>
+          <img src="/icons/Email.svg" onClick={handleSubmit}/>
+        </label>
+      </section>
+      <section></section>
     </>
   )
 }
