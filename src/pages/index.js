d75e4632a4e0d59b5dfa905d14c1c2ad75457d9f
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-06 12:34:37 -0300

    Main Container criado

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import Button from '../components/Button'
 
 // ICONS
 import { DiApple, DiAndroid } from "react-icons/di";
+import { FiPlay } from "react-icons/fi";
+
 
 export default function Home() {
   return (
@@ -33,6 +35,48 @@ export default function Home() {
         <img src="/icons/facebook.png" alt=""/>
         <img src="/icons/yahoo.png" alt=""/>
         <img src="/icons/paypal.png" alt=""/>
+      </section>
+      <section className={styles.mainContainer}>
+        <div className={styles.leftContent}>
+          <img src="/icons/Iphone.png" alt=""/>
+        </div>
+        <div className={styles.rightContent}>
+          <h1>Capture unforgettable moments.</h1>
+          <p>
+            Sapien eleifend diam fusce primis nec curabitur semper lorem dui cras pharetra 
+            consectetur cursus, ullamcorper curabitur habitant venenatis ultrices litora 
+            lectus eu tempus maecenas adipiscing. 
+          </p>
+          <div className={styles.appParticulars}>
+            <div>
+              <img src="/icons/Camera.svg" alt=""/>
+              <p>Elit eget pretium dapibus, eu.</p>
+            </div>
+            <div>
+              <img src="/icons/Video Camera.svg" alt=""/>
+              <p>Ligula posuere massa per, sodales.</p>
+            </div>
+            <div>
+              <img src="/icons/Eyeball.svg" alt=""/>
+              <p>Morbi quisque conubia lorem, ultrices.</p>
+            </div>
+            <div>
+              <img src="/icons/Video Camera.svg" alt=""/>
+              <p>Quis amet aptent nam, gravida.</p>
+            </div>
+            <div>
+              <img src="/icons/Location.svg" alt=""/>
+              <p>In congue curabitur convallis, tempus.</p>
+            </div>
+          </div>
+          <div className={styles.buttons}>
+            <Button>
+              <FiPlay size={20}/>
+              <span>Watch de Video</span>
+            </Button>
+            <a href="/">get the app</a>
+          </div>
+        </div>
       </section>
     </>
   )
