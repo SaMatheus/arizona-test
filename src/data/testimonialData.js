391828fc2c90071e4c7f11dc364109cf4fb3f9db
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-06 13:33:18 -0300

    criando testimonialData

---
+++ src/data/testimonialData.js
@@ -0,0 +1,28 @@
+const testimonialData = [
+  {
+    id: 1,
+    testimony: 'Viverra orci fringilla dolor ultrices nam blandit lobortis, aenean mi rhoncus rutrum vulputate nisi, cursus nisi et nec rhoncus arcu.',
+    author: 'Alexandra Monotolipo',
+    job: 'Software Developer'
+  },
+  {
+    id: 2,
+    testimony: 'Viverra orci fringilla dolor ultrices nam blandit lobortis, aenean mi rhoncus rutrum vulputate nisi, cursus nisi et nec rhoncus arcu.',
+    author: 'Peter Grill',
+    job: 'UI/UX Designer'
+  },
+  {
+    id: 3,
+    testimony: 'Viverra orci fringilla dolor ultrices nam blandit lobortis, aenean mi rhoncus rutrum vulputate nisi, cursus nisi et nec rhoncus arcu.',
+    author: 'Clovis Herrys',
+    job: 'Photographer'
+  },
+  {
+    id: 4,
+    testimony: 'Viverra orci fringilla dolor ultrices nam blandit lobortis, aenean mi rhoncus rutrum vulputate nisi, cursus nisi et nec rhoncus arcu.',
+    author: 'Lana DelQuenn',
+    job: 'CEO at Google'
+  },
+]
+
+export default testimonialData
