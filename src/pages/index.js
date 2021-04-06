2e40588f40b3df5ef1b69c224f29cad3a7555eb9
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-06 10:42:17 -0300

    Estilizando coponente Button

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,26 @@ import styles from '../styles/pages/Home.module.css'
 
 // COMPONENTS
 import Header from '../components/Header'
+import Button from '../components/Button'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <section className={styles.bannerContainer}>
+        <div className={styles.contentBox}>
+          <h1>Take Photos Like a Pro</h1>
+          <p>With only your iphone and capture app you can take stunning image li a professional Photographer</p>
+          <Button>
+            <img src="/icons/apple.svg" alt=""/>
+            <p>Iphone App</p>
+          </Button>
+          <Button>
+            <img src="/icons/android.svg" alt=""/>
+            <p>Android App</p>
+          </Button>
+        </div>
+      </section>
     </>
   )
 }
